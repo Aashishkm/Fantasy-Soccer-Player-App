f1170cc7c8d4794f3a641723ea30466a07f4fcff
amehra123 <60304331+amehra123@users.noreply.github.com>
2021-11-17 02:49:06 -0800

    GUI stuff

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,10 +1,9 @@
 package ui;
 
-import java.io.FileNotFoundException;
 //main class that runs everything
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        new FantasyTeamApp();
+    public static void main(String[] args)  {
+        new GUI();
     }
 }
